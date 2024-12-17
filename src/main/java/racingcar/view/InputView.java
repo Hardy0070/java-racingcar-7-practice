c71fb7c9802b8676144710af51e97533321ee274
HARDY0070 <hwv1219@gmail.com>
2024-12-17 23:05:14 +0900

    feat(view): 사용자 입력 검증 기능 추가 및 테스트 완료

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,9 @@
 package racingcar.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 //Input 객체
 //역할: 사용자로부터 입력을 받는 역할만 수행
 //책임: 입력값을 받아 처리하고, 필요한 형식으로 변환하거나 검증한다.
@@ -15,6 +18,29 @@ public class InputView {
 
     public int readRaceNumber() {
         return scanner.nextInt();
+    }
+
+    private String[] spiltCarNames(String carNames) {
+        return carNames.split(",");
+    }
+
+    private String removeSpaceLetter(String carName) {
+        String carNameAfterTrim = carName.trim();
+        if (carNameAfterTrim.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하로 입력해주세요.");
+        }
+        return carNameAfterTrim;
+    }
+
+    public List<String> validateCarNames(String carNames) {
+        List<String> validatedCarNames = new ArrayList<>();
+
+        String[] splitCarNames = spiltCarNames(carNames);
+        for (String splitCarName : splitCarNames) {
+            validatedCarNames.add(removeSpaceLetter(splitCarName));
+        }
+
+        return validatedCarNames;
     }
 
 }
